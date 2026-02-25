c8f57afccb4df0746e1c578ebeda5ba49299583b
Paul-Elvis-Otto <00_radium.cannoli@icloud.com>
2026-02-25 08:37:17 -0500

    fix: bawu-scraper

--- src/pollingapi/scraper/workers/sites_land/bawu.py
+++ src/pollingapi/scraper/workers/sites_land/bawu.py
@@ -22,14 +22,12 @@ class BawuScraper(WahlrechtLandScraper):
                 {
                     "url": "https://www.wahlrecht.de/umfragen/landtage/baden-wuerttemberg.htm",
                     "table_index": 0,
-                    "drop_header": 1,
                     "drop_footer": 3,
                     "table_id": "current",
                 },
                 {
                     "url": "https://www.wahlrecht.de/umfragen/landtage/baden-wuerttemberg.htm",
                     "table_index": 1,
-                    "drop_header": 1,
                     "drop_footer": 3,
                     "table_id": "historical",
                 },
